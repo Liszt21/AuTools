726b7adbe4d011e5db299ee4ba9e6f92ad34c63e
Liszt21 <1832666492@qq.com>
2020-08-29 20:14:44 +0800

    update windows setup

--- script/system/init/windows.ps1
+++ script/system/init/windows.ps1
@@ -1,12 +1,35 @@
 # 安装scoop
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-$env:SCOOP='C:\Liszt\scoop'
-[environment]::setEnvironmentVariable('SCOOP',$env:SCOOP,'User')
+
+if ($ENV:USERNAME -eq 'liszt'){
+    $ISME = $true
+}else{
+    $ISME = $false
+}
+
+if ($ISME) {
+    $env:SCOOP='C:\Liszt\scoop'
+    [environment]::setEnvironmentVariable('SCOOP',$env:SCOOP,'User')
+}
+
 Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 # Scoop basic setting
 scoop install git
 scoop bucket add extras
 
-# Install basic applications
-scoop install eamcs vim shadowsocks vscode listary 
+if ($ISME){
+    scoop bucket add java nerd-fonts
+
+    scoop install sudo ln
+    scoop install googlechrome anki bandizip mobaxterm porplayer sumatrapdf snipaste v2ray v2rayn flux dismplusplus
+    scoop install vscode atom emacs github julia vcxsrv
+    scoop install steam uplay
+
+    scoop install firacode 
+
+    git clone https://github.com/Liszt21/Dragon C:\Liszt\Projects\Dragon
+    sudo ln -s C:\Liszt\Projects\Dragon C:\Liszt\Scoop\buckets\dragon
+    scoop install lemacs autools
+    lemacs
+}
